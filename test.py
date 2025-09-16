8d778375ffefe4d1ac26cc5cab017928147bb187
conny <conny_jhj@naver.com>
2025-09-16 17:21:55 +0900

    feat 가변인자

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print("hi")
+# def profile(name,age,lang1,lang2,lang3,lang4,lang5):
+#     print("이름: {0}\t나이: {1}\t".format(name, age), end=" ")
+#     print(lang1,lang2,lang3,lang4,lang5)
+def profile(name,age,*language):
+    print("이름: {0}\t나이: {1}\t".format(name, age), end=" ")
+    for lang in language:
+        print(lang, end=" ")
+    print()
+
+
+profile("전현준",20,"Python","Java","C","C++","C#","JavaSript")
+profile("박서린",25, "Kotlin","Swift")
