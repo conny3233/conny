5d01706992b0d00d6358df5ac6a8c091a669c0cf
conny <conny_jhj@naver.com>
2025-09-16 18:45:33 +0900

    Quiz

--- test.py
+++ test.py
@@ -1,19 +1,22 @@
-gun = 10
+# 표준 체중을 구하는 프로그램 작성
+# 남자: 키*키*22
+# 여자: 키*키*21
 
-def checkpoint(soldiers):
-    global gun #전역변수
-    gun = gun - soldiers
-    print("[함수 내] 남은 총: {0}".format(gun))
+# 조건1: 표준체중은 별도의 함수 내에서 계산
+# 조건2: 표준체중은 소수점 둘째자리까지 표시
 
-def checkpoint_ret(gun,soldiers):
-    gun = gun - soldiers
-    print("[함수 내] 남은 총: {0}".format(gun))
-    return gun
+# ex) 키 175cm 남자의 표준 체중은 67.38kg입니다
 
 
-print("전체 총: {0}".format(gun))
-#checkpoint(2)
-gun = checkpoint_ret(gun,2)
-print("남은 총: {0}".format(gun))
-gun = checkpoint_ret(gun,2)
-print("남은 총: {0}".format(gun))
+def std_weight(height,gender):
+    if gender == "남자":
+        return (height/100)**2*22
+    else:
+        return (height/100)**2*21
+
+print("키를 입력해주세요: ") 
+height = float(input())
+print("성별을 입력해주세요: ") 
+gender = str(input())
+weight = std_weight(height,gender)
+print("키 {0:.0f}cm {1}의 표준 체중은 {2:.2f}kg입니다".format(height,gender,weight))
