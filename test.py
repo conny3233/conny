98a16e479fa9b389770ba9742b3e4437d8696ce9
conny <conny_jhj@naver.com>
2025-09-16 18:24:27 +0900

    feat: 지역변수 전역변수

--- test.py
+++ test.py
@@ -1,12 +1,19 @@
-# def profile(name,age,lang1,lang2,lang3,lang4,lang5):
-#     print("이름: {0}\t나이: {1}\t".format(name, age), end=" ")
-#     print(lang1,lang2,lang3,lang4,lang5)
-def profile(name,age,*language):
-    print("이름: {0}\t나이: {1}\t".format(name, age), end=" ")
-    for lang in language:
-        print(lang, end=" ")
-    print()
+gun = 10
+
+def checkpoint(soldiers):
+    global gun #전역변수
+    gun = gun - soldiers
+    print("[함수 내] 남은 총: {0}".format(gun))
+
+def checkpoint_ret(gun,soldiers):
+    gun = gun - soldiers
+    print("[함수 내] 남은 총: {0}".format(gun))
+    return gun
 
 
-profile("전현준",20,"Python","Java","C","C++","C#","JavaSript")
-profile("박서린",25, "Kotlin","Swift")
+print("전체 총: {0}".format(gun))
+#checkpoint(2)
+gun = checkpoint_ret(gun,2)
+print("남은 총: {0}".format(gun))
+gun = checkpoint_ret(gun,2)
+print("남은 총: {0}".format(gun))
